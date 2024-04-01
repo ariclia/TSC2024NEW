402da607c7429c78edae711e09f2ba20ab6e7823
ariclia <100340406+ariclia@users.noreply.github.com>
2024-04-01 09:20:46 +0300

    Update top.sv

--- LAB 3/lab01_testbench-interface/top.sv
+++ LAB 3/lab01_testbench-interface/top.sv
@@ -1,60 +1,58 @@
-/***********************************************************************
+/*************************
  * A SystemVerilog top-level netlist to connect testbench to DUT
- **********************************************************************/
+ ************************/
 
-module top;
- timeunit 1ns/1ns; //directiva de compilator
+module top; //metoda de incapsulare a codului 
+  timeunit 1ns/1ns; //o nano scunda cu o rezoutie de o nano secunda 
 
   // user-defined types are defined in instr_register_pkg.sv
-  import instr_register_pkg::*;
+  import instr_register_pkg::*; //avem nevoie de ceva din paketul asta
 
   // clock variables
-  logic clk; // stari 0,1,x-nu stiu daca e 0 sau 1,z-inalta impedanta
-  logic test_clk;
-
-  tb_ifc interf(clk, reset_n);
+  logic clk;     //wier 
+  logic test_clk; 
 
   // interconnecting signals
-  //logic          load_en;
-  //logic          reset_n;
-  //opcode_t       opcode;
-  //operand_t      operand_a, operand_b;
- // address_t      write_pointer, read_pointer;
-  //instruction_t  instruction_word;
+  logic          load_en; 
+  logic          reset_n;
+  opcode_t       opcode; //_t = template  
+  operand_t      operand_a, operand_b;
+  address_t      write_pointer, read_pointer;
+  instruction_t  instruction_word;
+  result_t       result;
 
   // instantiate testbench and connect ports
   instr_register_test test (
-    .interf(intf)
-    //.clk(test_clk),
-    //.load_en(load_en),
-    //.reset_n(reset_n),
-    //.operand_a(operand_a),
-    //.operand_b(operand_b),
-    //.opcode(opcode),
-   // .write_pointer(write_pointer),
-    //.read_pointer(read_pointer),
-    //.instruction_word(instruction_word)
+    .clk(test_clk),
+    .load_en(load_en),
+    .reset_n(reset_n),
+    .operand_a(operand_a),
+    .operand_b(operand_b),
+    .opcode(opcode),
+    .write_pointer(write_pointer),
+    .read_pointer(read_pointer),
+    .instruction_word(instruction_word),
+    .instruction_word_rez(result)
    );
 
   // instantiate design and connect ports
-  //module instr_register test instantiaza dut
   instr_register dut (
-    .interf(intf)
-    //.clk(clk),
-    //.load_en(load_en),
-   // .reset_n(reset_n),
-    //.operand_a(operand_a),
-    //.operand_b(operand_b),
-   // .opcode(opcode),
-    //.write_pointer(write_pointer),
-    //.read_pointer(read_pointer),
-   // .instruction_word(instruction_word)
-   );
-
+    .clk(clk),
+    .load_en(load_en),
+    .reset_n(reset_n),
+    .operand_a(operand_a),
+    .operand_b(operand_b),
+    .result(result),
+    .opcode(opcode),
+    .write_pointer(write_pointer),
+    .read_pointer(read_pointer),
+    .instruction_word(instruction_word)
+   ); 
+  //mai sus am conectat dut cu test
   // clock oscillators
-  initial begin
-    clk <= 0; // se atribuie 0
-    forever #5  clk = ~clk;
+  initial begin // tot codul o sa fie la timpul 0 facut 
+    clk <= 0; //clok ul incepe din 0 
+    forever #5  clk = ~clk; // cum afli frecventa unui semnal? Factorul de umplere? 
   end
 
   initial begin
